9948baa7731dd5ff620629339ccc5d18a6356d05
henry039 <henrylauyu039@gmail.com>
2018-11-05 00:10:55 +0800

    day 06 improved

--- 06-obj/f.js
+++ 06-obj/f.js
@@ -2,21 +2,18 @@ const char = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
 
 function transform(input) {
     if (typeof(input) === "number") {
-        let len =  input.toString().split('')
-        return word(input, len)
+        let adjust =  input.toString().split('')
+        return word(adjust)
     }else if (typeof(input) === "string"){
-        let len =  input.split('')
-        return word(input, len)
+        let adjust =  input.split('')
+        return word(adjust)
     }
 }
-function word(input, output) {
-    let temp = [];
+function word(input) {
     let word = [];
-    for (i in output){
-        temp[i] = parseInt(input %10)
-        input /=10 ;
-    }
-    temp.sort().map(charIndex =>{word.push(char[charIndex-1])});   
+    input.sort().map(charIndex =>{
+        word.push(char[parseInt(charIndex - 1)])
+    });
     return word.join('');  
 }
 
