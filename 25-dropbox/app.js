11d9235c99d0a1d25c8d88069ab143533e83151c
henry039 <henrylauyu039@gmail.com>
2018-11-05 00:17:36 +0800

    err msg added

--- 25-dropbox/app.js
+++ 25-dropbox/app.js
@@ -11,19 +11,24 @@ app.use(upload())
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
-app.get('/files/:name', (req, res) => {
+app.get('/request/:name', (req, res) => {
     readFile(`${__dirname}/files/${req.params.name}`, (err, data) => {
         if (err) throw err;
-    }).then(file => { res.send(file)}).catch(err =>{ res.send(`<h1>DID you see that shit DickHead</h1> <h1>${err}</h1>`)})
+    }).then(file => {
+        res.send(file)
+    }).catch(err => {
+        res.send(`<h1>DID you see that shit DickHead</h1> <h1>${err}</h1>`)
+    })
 })
+
 app.post('/files', (req, res) => {
-    if(req.files.suckMyDick !== undefined){
+    if (req.files.suckMyDick !== undefined) {
         req.files.suckMyDick.mv(`./files/${req.files.suckMyDick.name}`)
         res.send(`
             <h1>Info Recieved</h1>
             <h2>Your file upload : ${req.files.suckMyDick.name}</h2>
         `)
-    }else{
+    } else {
         res.send(`
             <h1>Empty Upload is not allowed</h1>
             <h1>You litte Piece of Shit !!!</h1>
