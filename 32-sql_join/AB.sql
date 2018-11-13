63f4c5acaea169d3d5312dbb4adb76d1281d6e20
henry039 <henrylauyu039@gmail.com>
2018-11-13 17:14:48 +0800

    day 32 CD added

--- 32-sql_join/AB.sql
+++ 32-sql_join/AB.sql
@@ -2,4 +2,16 @@
 select citrus.name, citrus.color, stock.quantity from stock inner join citrus on citrus.id = stock.id and citrus.color='orange';
 
 -- B
-select * from stock full outer join citrus on citrus.id = stock.id;
+select * from stock inner join citrus on citrus.id = stock.id;
+
+-- C
+select 
+    stock.id,
+    stock.quantity,
+    stock.price,
+    citrus.name,
+    citrus.color,
+    citrus.taste
+into 
+    citrus_stock 
+from stock inner join citrus on citrus.id = stock.id;
