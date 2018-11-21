035a426e42156bad253da69ac565c170218b01d3
henry039 <henrylauyu039@gmail.com>
2018-11-21 21:05:22 +0800

    day 36 v3

--- 01-Back-End/36-redis/a.js
+++ 01-Back-End/36-redis/a.js
@@ -3,59 +3,63 @@ const axios = require('axios')
 const app = require('express')();
 
 let client = redis.createClient()
+const {promisify} = require('util')
+const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
+const setexAsync = promisify(client.setex).bind(client);
+const expireAsync = promisify(client.expire).bind(client);
 
 client.on('error', err => {
     console.log('error event - ' + err);
 })
 
-async function readLatestBlock(limit = undefined) {
+function readLatestBlock(limit = undefined) {
     let fullBlockQueue = [];
-    let res = await axios.get('https://blockchain.info/latestblock')
-    res.data.txIndexes.slice(0, limit).map(target => {
-        client.get(target, async(err, res) => {
-            if (err) { console.log(err) }
-            if (res === null) {
-                fullBlockQueue.push(target);
-                await blockDetail(target)
-            } else {
-                fullBlockQueue.push(target)
-                client.expire(target, 3 * 60, err => {
-                    if (err) { console.log(err) }
-                })
-            }
-            client.set('latestBlock', JSON.stringify(fullBlockQueue), err => {
-                if (err) { console.log(err) }
-            })
+    return axios.get('https://blockchain.info/latestblock').then(res=>{
+        let DBpromises = res.data.txIndexes.slice(0, limit).map(target => {
+            return getAsync(target).then(async reply=>{
+                if (reply === null) {
+                    // add individual block detail
+                    await blockDetail(target)
+                    return fullBlockQueue.push(target);
+                } else {
+                    // extend block detail life-time
+                    expireAsync(target, 3 * 60).then(()=>{
+                        return fullBlockQueue.push(target)
+                    })
+                }
+            }).catch(err=>console.log(err))
         })
-    })
-    
+        return Promise.all(DBpromises).then(()=>{
+            return setAsync('latestBlock', JSON.stringify(fullBlockQueue)).then(()=>{return extract()})
+        })
+    }).catch(err=>console.log(err))
 }
 
-async function blockDetail(inputIndex) {
-    let hash = await axios.get(`https://blockchain.info/rawtx/${inputIndex}`)
-    console.log(`Fetching the transaction detail ${inputIndex}`);
-    client.setex(inputIndex, 3 * 60, JSON.stringify(hash.data), err => {
-        if (err) { console.log(err) }
-    })
+// fetch individual block detail
+function blockDetail(inputIndex) {
+    return axios.get(`https://blockchain.info/rawtx/${inputIndex}`).then(hash=>{
+        console.log(`Fetching the transaction detail ${inputIndex}`);
+        return setexAsync(inputIndex, 3 * 60, JSON.stringify(hash.data))
+    }).catch(err=>console.log(err))
+}
+
+// output individual block needed detail 
+function extract(){
+    let hash = []
+    return getAsync('latestBlock').then(reply=>{
+        let blockArr = JSON.parse(reply)
+        let detail = blockArr.map(async index => {
+            await getAsync(index).then(reply=>{
+                hash.push(JSON.parse(reply).hash)
+            })
+        })
+        return Promise.all(detail).then(()=> {return hash})
+    }).catch(err=>console.log(err))
 }
 
 app.get('/lastestBlock', (req, res)=>{
-    readLatestBlock(5)
-    setTimeout(() => {
-        let hash = []
-        client.get('latestBlock', (err, reply) => {
-            if (err) { console.log(err) }
-            let blockArr = JSON.parse(reply)
-            blockArr.map(index => {
-                client.get(index, (err, reply) => {
-                    if (err) { console.log(err) }
-                    let a = JSON.parse(reply).hash;
-                    hash.push(a)
-                })
-            })
-            res.json(hash)
-        })
-    }, 5000);
+    readLatestBlock(55).then(data=>res.json(data))
 })
 
 app.listen(8080, ()=>{
